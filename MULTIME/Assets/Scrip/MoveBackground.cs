c125abc3ca484626680eb73d646b6217bc5629d3
tansan47398 <tan.nantaphop@gmail.com>
2022-08-30 18:47:47 +0700

    title bg ghange
    scrolling space bg

---
+++ MULTIME/Assets/Scrip/MoveBackground.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    public Rigidbody2D RB;
+    public BoxCollider2D Collider;
+
+    public float speed = -1.5f;
+
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        Collider = GetComponent<BoxCollider2D>();
+
+        Collider.enabled = false;
+
+        RB.velocity = new Vector2(speed, 0);
+    }
+
+    void Update()
+    {
+        if (transform.position.x < -19)
+        {
+            transform.position =new  Vector2(19, 0);
+        }
+    }
+}
