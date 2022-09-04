a5bb98038b64c7d13df21fe74b8b1db39eab24bd
tansan47398 <tan.nantaphop@gmail.com>
2022-09-04 16:08:02 +0700

    space ship control and  BGspace

--- MULTIME/Assets/Scrip/ParallaxBackground.cs
+++ MULTIME/Assets/Scrip/ParallaxBackground.cs
@@ -4,27 +4,41 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
-    private float length, startpos;
+    private float length, startpos, hight, startposy;
     public GameObject cam;
-    public float parallaxEffect;
+    public float parallaxEffect,parallaxEffecty;
 
     void Start()
     {
         startpos = transform.position.x;
+        startposy = transform.position.y;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
+        hight = GetComponent<SpriteRenderer>().bounds.size.y;
     }
     void Update()
     {
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
 
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        float tempy = (cam.transform.position.y * (1 - parallaxEffecty));
+        float disty = (cam.transform.position.y * parallaxEffecty);
+
+        transform.position = new Vector3(startpos + dist, startposy - disty, transform.position.z);
         if (temp > startpos+length)
         {
             startpos += length;
-        }else if(temp<startpos-length)
+        }
+        else if(temp < startpos-length)
         {
             startpos -= length;
+        }
+        else if (tempy > startposy + hight)
+        {
+            startposy += hight;
+        }
+        else if (tempy < startposy -hight)
+        {
+            startposy -= hight;
         }
     }
 }
--- MULTIME/Assets/Scrip/SpaceshipControl.cs
+++ MULTIME/Assets/Scrip/SpaceshipControl.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class SpaceshipControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float horizontal;
+    float vertical;
+    private Rigidbody2D rb;
+    public float speed;
+    private Transform BodyTansform;
+    public float size;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        BodyTansform = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector2(horizontal * Time.deltaTime*speed, vertical * Time.deltaTime*speed);
+        if (horizontal != 0f)
+        {
+            BodyTansform.localScale = new Vector3(horizontal * size, size,size);
+        }
     }
 }
